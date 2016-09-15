d6d42d91869a47d3ea05d196fe22f502d32945ee
dtw <dtw2005@gmail.com>
2016-09-15 11:38:42 -0400

    modulize angular js, move directives to products.js

--- app.js
+++ app.js
@@ -2,33 +2,11 @@
 (function () {
     "use strict";
 
-    var app = angular.module('gemStore', []);
+    var app = angular.module('gemStore', ['store-products']);
 
     app.controller('StoreController', function () {
         this.products = gems;
     });
-
-    // app.controller('TabController', function () {
-    //     this.tab = 1;
-    //
-    //     this.setTab = function (newValue) {
-    //         this.tab = newValue;
-    //     };
-    //
-    //     this.isSet = function (tabName) {
-    //         return this.tab === tabName;
-    //     };
-    // });
-
-
-    // app.controller('GalleryController', function () {
-    //     this.current = 0;
-    //
-    //     this.setCurrent = function (newValue) {
-    //         this.current = newValue || 0;
-    //     }
-    // });
-
 
     app.controller("ReviewController", function () {
         this.review = {};
@@ -40,55 +18,6 @@
         };
     });
 
-    app.directive("productDescription", function () {
-        return {
-            restrict: "E",
-            templateUrl: "product-description.html"
-        };
-    });
-
-
-    app.directive("productSpecs", function () {
-        return {
-            restrict: "A",
-            templateUrl: "product-specs.html"
-        };
-    });
-
-    app.directive("productTabs", function () {
-        return {
-            restrict: "E",
-            templateUrl: "product-tabs.html",
-            controller: function () {
-                this.tab = 1;
-
-                this.setTab = function (newValue) {
-                    this.tab = newValue;
-                };
-
-                this.isSet = function (tabName) {
-                    return this.tab === tabName;
-                };
-            },
-            controllerAs: 'tab'
-        };
-    });
-
-
-    app.directive("productGallery", function () {
-        return {
-            restrict: "E",
-            templateUrl: "product-gallery.html",
-            controller: function () {
-                this.current = 0;
-
-                this.setCurrent = function (newValue) {
-                    this.current = newValue || 0;
-                };
-            },
-            controllerAs: 'gallery'
-        };
-    });
 
     var gems = [
         {
